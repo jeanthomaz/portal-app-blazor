80aa632fe9378ed29f57e3da0f96210207f7640e
daniloflorenzano <danilo@directdigital.com.br>
2023-09-17 12:08:49 -0300

    corrige bug de ficar criando um arquivo de banco de dados fora do projeto de persistencia

--- Persistence/ApplicationDbContext.cs
+++ Persistence/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
 
-public class ApplicationDbContext  : DbContext
+public class ApplicationDbContext : DbContext
 {
     public DbSet<Presentation> Presentations { get; set; }
     public DbSet<Student> Students { get; set; }
@@ -13,11 +13,14 @@ public class ApplicationDbContext  : DbContext
     public ApplicationDbContext()
     {
     }
-    
-    public ApplicationDbContext (DbContextOptions<ApplicationDbContext > options) : base(options)
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlite("Data Source=mydb.db;");
+        => optionsBuilder.UseSqlite($"Data Source={GetDatabasePath()};");
+
+    private string GetDatabasePath()
+        => Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())!.FullName, "Persistence/mydb.db");
 }
